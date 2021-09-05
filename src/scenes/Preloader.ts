2cbe66acc866d03be7dafdaf4d8509e60962539b
davoid-avoid <davetrowe@gmail.com>
2021-09-05 15:12:19 -0400

    still a bug on mobile, preloader sometimes doesn't change scenes

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -6,21 +6,21 @@ export default class Preloader extends Phaser.Scene {
   }
 
   preload() {
-      let wRatio;
-      let offsetRatio;
-      let offsetRatioText;
-      let offsetRatioMobileRect;
-      if (window.innerWidth > 400){
-          wRatio = window.innerWidth / 5;
-          offsetRatio = 120;
-          offsetRatioText = 0;
-          offsetRatioMobileRect = 0;
-      } else {
-          wRatio = 30;
-          offsetRatio = 100;
-          offsetRatioText = window.innerWidth / 2;
-          offsetRatioMobileRect = 140;
-      }
+    let wRatio;
+    let offsetRatio;
+    let offsetRatioText;
+    let offsetRatioMobileRect;
+    if (window.innerWidth > 400) {
+      wRatio = window.innerWidth / 5;
+      offsetRatio = 120;
+      offsetRatioText = 0;
+      offsetRatioMobileRect = 0;
+    } else {
+      wRatio = 30;
+      offsetRatio = 100;
+      offsetRatioText = window.innerWidth / 2;
+      offsetRatioMobileRect = 140;
+    }
     const text1 = this.add.text(wRatio, 220, "LOADING SYSTEM", {
       fontFamily: "Helvetica",
       fontStyle: "bold",
@@ -30,17 +30,17 @@ export default class Preloader extends Phaser.Scene {
     let progressBars = [];
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 3; j++) {
-        let x = wRatio * (j + 1) + (j * offsetRatioText) + 100;
+        let x = wRatio * (j + 1) + j * offsetRatioText + 100;
         let y = 270 + i * 30 + i * 10;
         let w = wRatio - offsetRatio + offsetRatioMobileRect;
         let h = 30;
         var progressBox = this.add.rectangle(x, y, w, h, 0xff0000);
-        progressBox.setOrigin(0, 0)
+        progressBox.setOrigin(0, 0);
         progressBox.setData("loadValue", i / 10);
         progressBox.setData("loaded", false);
         console.log(progressBox);
         var text = this.add.text(
-          wRatio * (j + 1) + (j * offsetRatioText),
+          wRatio * (j + 1) + j * offsetRatioText,
           276 + i * 30 + i * 10,
           "LDSYS-" + j + "-" + i,
           { fontFamily: "Helvetica", fontStyle: "bold", color: "#ff6600" }
@@ -62,7 +62,6 @@ export default class Preloader extends Phaser.Scene {
     this.load.atlas("flameSprite", "sprites/flame.png", "sprites/flame.json");
     this.load.atlas("starSprite", "sprites/star.png", "sprites/star.json");
 
-    let self = this;
     this.load.on("progress", function (value) {
       progressBars.forEach((bar) => {
         if (bar.getData("loadValue") < value) {
@@ -74,17 +73,27 @@ export default class Preloader extends Phaser.Scene {
     this.load.on("fileprogress", function (file) {
       console.log(file.src);
     });
+
+    let self = this;
     this.load.on("complete", function () {
       console.log("complete");
-      text1.text = "LOAD COMPLETE"
-      setInterval(function(){
+      text1.text = "LOAD COMPLETE";
+      let flashes = 0;
+      let flashInterval = setInterval(function () {
         text1.alpha = !text1.alpha;
-      }, 100)
-      setTimeout(function(){
-        self.scene.start("portfolio");
-      }, 1100)
+        flashes++;
+        if (flashes > 16) {
+          clearInterval(flashInterval);
+          text1.destroy();
+          progressBars.forEach((bar) => {
+            bar.destroy();
+          });
+          self.scene.start("portfolio");
+        }
+      }, 100);
     });
   }
   create() {
+    //this.scene.start("portfolio");
   }
 }

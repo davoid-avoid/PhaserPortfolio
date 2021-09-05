46b230d29be29a6bff71d5b031addc56408049dc
davoid-avoid <davetrowe@gmail.com>
2021-09-05 14:39:49 -0400

    creating preloader

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,23 +1,82 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
-export default class Preloader extends Phaser.Scene
-{
-    constructor(){
-        super('preloader')
-    }
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super("preloader");
+  }
 
-    preload(){
-        this.load.image('tiles', 'tiles/tilemaplargeextrudetest.png')
-        this.load.image('clouds', 'tiles/clouds.png')
-        this.load.tilemapTiledJSON('tilemap', 'tiles/tilemap.json')
-        
-        this.load.atlas('characterSprite', 'sprites/character.png', 'sprites/character.json')
-        this.load.atlas('arrowSprite', 'sprites/arrow.png', 'sprites/arrow.json')
-        this.load.atlas('birdSprite', 'sprites/bird.png', 'sprites/bird.json')
-        this.load.atlas('flameSprite', 'sprites/flame.png', 'sprites/flame.json')
-        this.load.atlas('starSprite', 'sprites/star.png', 'sprites/star.json')
+  preload() {
+      let wRatio;
+      if (window.innerWidth > 400){
+          wRatio = window.innerWidth / 5
+      } else {
+          wRatio = 150;
+      }
+    const text1 = this.add.text(wRatio, 220, "LOADING SYSTEM", {
+      fontFamily: "Helvetica",
+      fontStyle: "bold",
+      color: "#ff6600",
+      fontSize: 30,
+    });
+    let progressBars = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 3; j++) {
+        let x = wRatio * (j + 1) + 100;
+        let y = 270 + i * 30 + i * 10;
+        let w = wRatio - 120;
+        let h = 30;
+        var progressBox = this.add.rectangle(x, y, w, h, 0xff0000);
+        progressBox.setOrigin(0, 0)
+        progressBox.setData("loadValue", i / 10);
+        progressBox.setData("loaded", false);
+        console.log(progressBox);
+        var text = this.add.text(
+          wRatio * (j + 1),
+          276 + i * 30 + i * 10,
+          "LDSYS-" + j + "-" + i,
+          { fontFamily: "Helvetica", fontStyle: "bold", color: "#ff6600" }
+        );
+        progressBars.push(progressBox);
+      }
     }
-    create(){
-        this.scene.start('portfolio')
-    }
+    this.load.image("tiles", "tiles/tilemaplargeextrudetest.png");
+    this.load.image("clouds", "tiles/clouds.png");
+    this.load.tilemapTiledJSON("tilemap", "tiles/tilemap.json");
+
+    this.load.atlas(
+      "characterSprite",
+      "sprites/character.png",
+      "sprites/character.json"
+    );
+    this.load.atlas("arrowSprite", "sprites/arrow.png", "sprites/arrow.json");
+    this.load.atlas("birdSprite", "sprites/bird.png", "sprites/bird.json");
+    this.load.atlas("flameSprite", "sprites/flame.png", "sprites/flame.json");
+    this.load.atlas("starSprite", "sprites/star.png", "sprites/star.json");
+
+    let self = this;
+    this.load.on("progress", function (value) {
+      progressBars.forEach((bar) => {
+        if (bar.getData("loadValue") < value) {
+          bar.setFillStyle(0x249225);
+        }
+      });
+    });
+
+    this.load.on("fileprogress", function (file) {
+      console.log(file.src);
+    });
+    this.load.on("complete", function () {
+      console.log("complete");
+      text1.text = "LOAD COMPLETE"
+      setInterval(function(){
+        text1.alpha = !text1.alpha;
+
+      }, 100)
+      setTimeout(function(){
+        self.scene.start("portfolio");
+      }, 1100)
+    });
+  }
+  create() {
+  }
 }
